e61892fe43520376499193c785fc07fd7ccade59
Bilal Haidar <bhaidar@gmail.com>
2019-11-08 11:10:05 +0200

    fix footer

--- j-tek/src/components/Footer/Footer.js
+++ j-tek/src/components/Footer/Footer.js
@@ -3,13 +3,12 @@ import "./Footer.css";
 
 const Footer = () => {
     return (
-        
-            <div className="footer-copyright text-center">
-            &copy; {new Date().getFullYear()} Copyright:{" J-TEK LAW PLLC"} | {"Privacy Policy"} | (703) 896-7960
+        <footer>
+            <div className="footer-copyright">
+                &copy; {new Date().getFullYear()} Copyright:{" J-TEK LAW PLLC"} | {"Privacy Policy"} | (703) 896-7960
             </div>
-       
-  
-        )
+        </footer>
+    )
 }
 
 export default Footer;
